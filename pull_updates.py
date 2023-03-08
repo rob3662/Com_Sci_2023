d00192c3e2042963b8a34cecc1ddda451c46509d
Robert Brake <92334983+rob3662@users.noreply.github.com>
2023-03-07 23:08:46 -0330

    Add files via upload

---
+++ pull_updates.py
@@ -0,0 +1,9 @@
+#You can run this file to get the updates instead of
+#entering the commands in the terminal.
+#
+#You can copy it to your desktop if you want, or keep it here
+
+import os
+
+os.system('cd /home/pi/com_sci_2023')
+os.system('git pull')
