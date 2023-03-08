730d1d2b2d986ed1afa8012b426c1d3d56a4def2
Robert Brake <92334983+rob3662@users.noreply.github.com>
2023-03-08 19:58:38 -0330

    Create get_updates.py

---
+++ get_updates.py
@@ -0,0 +1,8 @@
+'''
+You can open this file in your python editor and run it.
+It will download any new items that I have posted on github.
+'''
+
+import os
+os.system('cd /com_sci_2023')
+os.system('git pull')
